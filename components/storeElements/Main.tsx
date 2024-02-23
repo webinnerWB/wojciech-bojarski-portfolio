34c512815ce8c1dcf80cce3605a0e1a2a3915b36
webinner.wojciech.bojarski@gmail.com <webinnerWB>
2024-02-23 23:25:01 +0100

    filters done

--- components/storeElements/Main.tsx
+++ components/storeElements/Main.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ChangeEvent, useRef, useEffect } from "react"
+import React, { FC, ChangeEvent, useRef, useEffect, useState, FormEvent } from "react"
 
 import style from '../../style/store.module.scss'
 
@@ -9,12 +9,35 @@ type mainComponent = {
 
 const Main: FC<mainComponent> = ({handleSearchingValue, handleSearchResults}: mainComponent) => {
     const searchIntup = useRef<HTMLInputElement|null>(null)
+    const [inputValue, setInputValue] = useState<string>('')
+
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        handleSearchResults(inputValue)
+        clearInpit()
+    }
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        handleSearchingValue(e)
+        setInputValue(e.target.value)
+    }
+
+    const clearInpit = () => {
+        const input = searchIntup.current
+
+        if(input) {
+            input.value = ''
+        }
+    }
+
     useEffect(() => {
         const input = searchIntup.current
         if(input) {
             input.addEventListener('keypress', (event) => {
                 if (event.key === 'Enter') {
                     handleSearchResults(input.value)
+                    clearInpit()
                 }
               });
         }
@@ -25,15 +48,17 @@ const Main: FC<mainComponent> = ({handleSearchingValue, handleSearchResults}: ma
             <div className={`${style.mainWrapper}`}>
                 <h1 className={`${style.title}`}>Find the perfect product with petishop</h1>
                 <div className={`${style.searchMainWrapper}`}>
-                    <input 
-                    className={`${style.inputSearch} 
-                    ${style.inputSearchMain}`} 
-                    placeholder="Search for products" 
-                    onChange={handleSearchingValue}
-                    ref={searchIntup}
-                    />
-                    <i className={`fa-solid fa-magnifying-glass ${style.iconSearchMain}`}></i>
-                    <button className={`${style.searchBtn}`}>Search</button>
+                    <form onSubmit={handleSubmit}>
+                        <input 
+                            className={`${style.inputSearch} 
+                            ${style.inputSearchMain}`} 
+                            placeholder="Search for products" 
+                            onChange={handleChange}
+                            ref={searchIntup}
+                        />
+                        <i className={`fa-solid fa-magnifying-glass ${style.iconSearchMain}`}></i>
+                        <button type="submit" className={`${style.searchBtn}`}>Search</button>
+                    </form>
                 </div>
             </div>
             <img className={`${style.digi} ${style.secPosition}`} src="/web_store/digi.png" />
